90cd7b69f0c0bdf4268c9483ee15a59e592a515f
Poonam <waghpoonam799@gmail.com>
2024-07-17 17:47:02 +0530

    First commit

---
+++ simpleint.java
@@ -0,0 +1,7 @@
+public class simpleint {
+    public static void main(String[] args) {
+        float P = 1, T = 1, R = 1;
+        float SI = (P * T * R) / 100;
+        System.out.println("Simple interest is " + SI);
+    }
+}
