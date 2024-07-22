c3027ee02bb619fc7b90a2fa66b6e61b5ae44f1f
Poonam <waghpoonam799@gmail.com>
2024-07-22 18:11:43 +0530

    First commit

---
+++ largeelement.java
@@ -0,0 +1,16 @@
+public class largeelement {
+    static int arr[] = { 10, 324, 45, 90, 9808 };
+
+    static int largest() {
+        int i;
+        int max = arr[0];
+        for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = arr[i];
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("largest array is " + largest());
+    }
+}
