d552a129821ab17b9abd69e3d4683983017d50ca
Poonam <waghpoonam799@gmail.com>
2024-07-25 16:31:23 +0530

    First commit

---
+++ stringtodouble.java
@@ -0,0 +1,7 @@
+public class stringtodouble {
+    public static void main(String[] args) {
+        String str = "203.1224";
+        double str1 = Double.parseDouble(str);
+        System.out.println(str1);
+    }
+}
