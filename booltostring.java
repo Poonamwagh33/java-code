ecb316f707d7411d773b0c6de31e4024112a66ed
Poonam <waghpoonam799@gmail.com>
2024-07-25 16:32:44 +0530

    First commit

---
+++ booltostring.java
@@ -0,0 +1,7 @@
+public class booltostring {
+    public static void main(String[] args) {
+        boolean value = true;
+        String output = Boolean.toString(value);
+        System.out.println(output);
+    }
+}
