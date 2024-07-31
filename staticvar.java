89edae0a9e5c3c73ddc651196025e22768cb2a37
Poonam <waghpoonam799@gmail.com>
2024-07-31 17:25:20 +0530

    First commit

---
+++ staticvar.java
@@ -0,0 +1,17 @@
+public class staticvar {
+    static int a = m1();
+    static {
+        System.out.println("inside static block");
+    }
+
+    static int m1() {
+        System.out.println("from m1");
+
+        return 20;
+    }
+
+    public static void main() {
+        System.out.println("value of a:" + a);
+        System.out.println("from main");
+    }
+}
