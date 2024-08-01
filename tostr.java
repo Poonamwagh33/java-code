1a3277d1aa1d5e2fdce10995dbda0cb1ac3036ed
Poonam <waghpoonam799@gmail.com>
2024-08-01 17:49:29 +0530

    First commit

---
+++ tostr.java
@@ -0,0 +1,6 @@
+public class tostr {
+    public static void main(String args[]) {
+        StringBuilder str = new StringBuilder("Poonam");
+        System.out.println("String contains =  " + str.toString());
+    }
+}
