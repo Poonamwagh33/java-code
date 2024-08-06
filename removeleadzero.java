823762d384f3c0e0762bf09d2809c3be5cbcca42
Poonam <waghpoonam799@gmail.com>
2024-08-06 17:51:15 +0530

    First commit

---
+++ removeleadzero.java
@@ -0,0 +1,16 @@
+public class removeleadzero {
+    public static String removeZero(String str) {
+        int i = 0;
+        while (i < str.length() && str.charAt(i) == '0')
+            i++;
+        StringBuffer sb = new StringBuffer(str);
+        sb.replace(0, i, " ");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "0000123678";
+        str = removeZero(str);
+        System.out.println(str);
+    }
+}
