4e3bff64e879e6b265b18575f5676135dfc6d854
Poonam <waghpoonam799@gmail.com>
2024-08-18 21:44:36 +0530

    First commit

---
+++ octaltodecimal.java
@@ -0,0 +1,7 @@
+public class octaltodecimal {
+    public static void main(String[] args) {
+        String onum = "157";
+        int num = Integer.parseInt(onum, 8);
+        System.out.println("decimal euivalent of octal value is:" + num);
+    }
+}
