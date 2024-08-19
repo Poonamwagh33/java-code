6bde1a2b9d05ca06fa4778daea0f8f7eeb9fb264
Poonam <waghpoonam799@gmail.com>
2024-08-19 15:51:09 +0530

    First commit

---
+++ decimaltooctal.java
@@ -0,0 +1,19 @@
+public class decimaltooctal {
+    static void decimaltooctal(int n) {
+        int[] octalNum = new int[100];
+        int i = 0;
+        while (n != 0) {
+            octalNum[i] = n % 8;
+            n = n / 8;
+            i++;
+        }
+        for (int j = i - 1; j >= 0; j--)
+            System.out.print(octalNum[j]);
+
+    }
+
+    public static void main(String[] args) {
+        int n = 33;
+        decimaltooctal(n);
+    }
+}
