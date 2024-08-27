2a8c018abd9ff086762fda708dba6e7479534f6a
Poonam <waghpoonam799@gmail.com>
2024-08-27 18:06:25 +0530

    First commit

---
+++ rmalloccur.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class rmalloccur {
+    public static int[] removElements(int[] arr, int key) {
+        int index = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] != key)
+                arr[index++] = arr[i];
+        return Arrays.copyOf(arr, index);
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 3, 9, 2, 3, 1, 7, 2, 3 };
+        int key = 3;
+        array = removElements(array, key);
+        System.out.println(Arrays.toString(array));
+    }
+}
