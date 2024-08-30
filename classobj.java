078e1d1ff3216c81c6c3d8c3f832122a32765ec8
Poonam <waghpoonam799@gmail.com>
2024-08-30 18:18:30 +0530

    First commit

---
+++ classobj.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+class classobj {
+    int id;
+    String name;
+
+    public static void main(String args[]) {
+        classobj c = new classobj();
+        System.out.println(c.id);
+        System.out.println(c.name);
+    }
+
+}
