abe4474c3cbc452637137570d58307e16691ea5d
Poonam <waghpoonam799@gmail.com>
2024-08-31 17:38:18 +0530

    First commit

---
+++ chartoint.java
@@ -0,0 +1,8 @@
+class chartoint {
+    public static void main(String[] args) {
+        char ch = '3';
+        System.out.println("char value:" + ch);
+        int a = ch - '0';
+        System.out.println("int value:" + a);
+    }
+}
