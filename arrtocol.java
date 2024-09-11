013049078dace722b48a998709c4d35e8ace8e6a
Poonam <waghpoonam799@gmail.com>
2024-09-11 18:36:38 +0530

    First commit

---
+++ arrtocol.java
@@ -0,0 +1,24 @@
+// Convert an Array into Collection in Java
+
+// import java util library
+import java.util.*;
+
+// class for writing logic of the problem
+public class arrtocol {
+    public static void main(String args[]) {
+        // array input
+        String playersArray[] = { "Virat", "Sachin", "Rohit", "Bumrah" };
+
+        // printing input elements for comparison
+        System.out.println("Array input: "
+                + Arrays.toString(playersArray));
+
+        // converting array into Collection
+        // with asList() function
+        List playersList = Arrays.asList(playersArray);
+
+        // print converted elements
+        System.out.println("Converted elements: "
+                + playersList);
+    }
+}
